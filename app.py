1506357b8d7bc9d277ba31a7a00f106f129ab885
Yiling Cao <yiling.cao@gmail.com>
2019-09-04 21:27:30 +0800

    add fisrt version.

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route('/')
+def index():
+    a=12312
+    return render_template('index.html', value=a)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
