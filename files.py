e91d24b2fb53923ac9fbc14717e0c36d7743e827
Yiling Cao <yiling.cao@gmail.com>
2019-09-12 19:57:28 +0800

    fix infobox data output croodinates error.

--- files.py
+++ files.py
@@ -1,9 +1,9 @@
 import glob
 import os
 import pathlib
-everything = pathlib.Path().glob("**/*.jpg")
+everything = pathlib.Path().glob("**/*.jpg").sort()
 
 cwd = os.getcwd()
-jpg_files = (glob.glob("**/*.jpg"))
+jpg_files = (glob.glob("**/*.jpg")) 
 for jpg in everything:
     print(jpg)
