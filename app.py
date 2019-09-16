5f911f00f144282b23ffac056c60cb3bf2055f65
Yiling Cao <yiling.cao@gmail.com>
2019-09-16 16:16:27 +0800

    able to upload bbox data to flask server.

--- app.py
+++ app.py
@@ -1,26 +1,45 @@
-from flask import Flask, render_template
+import json
+import os
+
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 Bootstrap(app)
 
-@app.route('/')
+
+@app.route('/', methods=["GET"])
 def index():
-    #with open('files.txt', 'r') as f:
-    #	a=f.read()
     return render_template('index.html', value=0)
+
+
+@app.route('/', methods=["POST"])
+def index_post():
+    data_ = request.get_data(as_text=True)
+    data = data_.split('.jpg')
+    txt = data[0] + ".txt"
+    result = data[1]
+
+    with open(txt, 'w') as f:
+        f.writelines(result)
+        
+    return "200 OK"
+
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
 
+
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
