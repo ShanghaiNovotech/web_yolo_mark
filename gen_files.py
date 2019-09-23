2d57e1bce0b14c3515a98063c5a66e8a045248c6
Yiling Cao <yiling.cao@gmail.com>
2019-09-23 15:29:31 +0800

    change exection on gen_files.py

--- gen_files.py
+++ gen_files.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python3
 import glob
 import os
 import pathlib
 
 jpg_file_paths = list(pathlib.Path().glob("**/*.jpg"))
-jpg_files = list(map(str, jpg_file_paths)).sort()
+jpg_files = sorted(list(map(str, jpg_file_paths)))
 
 for jpg in jpg_files:
     print(jpg)
